6d8e217005d55c90693216be551faecab2cd3fc9
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2024-01-28 10:20:43 +0300

    signin implemented

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,56 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { UserAuth } from "../../Context/AuthContext";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const onLogin = (e) => {
-       
-    };
-    return (
-        <>
-          <main>
-            <section>
-              <div className="login-page">
-                <h1>
-                  Budget <span>Tracker</span>
-                </h1>
-                <form action="">
-                  <div className="email-input">
-                    <label htmlFor="email-address">Email Address</label>
-                    <input
-                      type="email"
-                      id="email-address"
-                      name="email"
-                      required
-                      placeholder="Email Address"
-                    />
-                  </div>
-                  <div className="password-input">
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      required
-                      placeholder="Password"
-                    />
-                  </div>
-                  <div>
-                    <button className="btn-login" onClick={onLogin}>
-                      Login
-                    </button>
-                  </div>
-                </form>
-                <p>
-                  No account yet?
-                  <span>
-                    Sign up
-                  </span>
-                </p>
+  const { signIn } = UserAuth();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signIn(email, password);
+      console.log("User succesfully sign in");
+      navigate("/home");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <>
+      <main>
+        <section>
+          <div className="login-page">
+            <h1>
+              Budget <span>Tracker</span>
+            </h1>
+            <form action="">
+              <div className="email-input">
+                <label htmlFor="email-address">Email Address</label>
+                <input
+                  type="email"
+                  id="email-address"
+                  name="email"
+                  required
+                  placeholder="Email Address"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
-            </section>
-          </main>
-        </>
-      );
-}
+              <div className="password-input">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  required
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div>
+                <button className="btn-login" onClick={handleLogin}>
+                  Login
+                </button>
+              </div>
+            </form>
+            <p>
+              No account yet?
+              <span>
+                <NavLink to={"/"}>Sign up</NavLink>
+              </span>
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
 
-export default Login
+export default Login;
