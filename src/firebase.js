544345dbdc4644a13b56bff97824c75968563b04
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2024-01-28 09:19:33 +0300

    firebase added to the project

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgN_QXQjmxC3BLE9wl7GofrvRHiOoO1tI",
+  authDomain: "xpenditure-af200.firebaseapp.com",
+  projectId: "xpenditure-af200",
+  storageBucket: "xpenditure-af200.appspot.com",
+  messagingSenderId: "689539806134",
+  appId: "1:689539806134:web:b8138b402378bddef54717"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
